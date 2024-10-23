d7ef65712544588ce8cc70dfc779983cdb6f2ec8
Stanislav Provotorov <stas_provotorov@Stanislavs-MBP.home>
2024-10-24 00:52:05 +0400

    Send welcome message with lang

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ import telebot
 
 load_dotenv('tg_bot_token.env')
 TOKEN = getenv('TOKEN')
+bot_lang = 'en'
 
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    print(message.from_user.language_code)
-    bot.send_message(message.chat.id, 'Hello!')
+    global bot_lang
+    user_lang = message.from_user.language_code
+    bot_lang = user_lang
+    message_back = {
+        'en': 'Welcome! Your language is set to English',
+        'ru': 'Добро пожаловать! Ваш язык установлен на русский'
+        }
+    bot.send_message(message.chat.id, message_back[user_lang])
 
 
 bot.polling(none_stop=True)
