22c1f3375b1d4c78b573f3484d5c473bd7ed8afc
Stanislav Provotorov <stas_provotorov@Stanislavs-MBP.home>
2024-10-24 22:50:18 +0400

    Writed User and Users classes

---
+++ sessions.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+class User:
+    def __init__(self, language):
+        self.language = language
+        self.start_time = datetime.now()
+
+    @property
+    def language(self):
+        return self._language
+    
+    @language.setter
+    def language(self, value):
+        self._language = value
+
+
+class Users:
+    def __init__(self):
+        self.users = {}
+
+    def __getitem__(self, id):
+        if id in self.users:
+            return self.users[id]
+        raise KeyError(f'The user with this ID={id} is not found')
+
+    
+    def __setitem__(self, id, user_obj):
+        if isinstance(user_obj, User):
+            self.users[id] = user_obj
+        else:
+            raise TypeError('Value must be an instance of User class')
