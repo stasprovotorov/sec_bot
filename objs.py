abf36866ab98f05ba367ee1e6cbdb9c944c31173
Stanislav Provotorov <stas_provotorov@Stanislavs-MBP.home>
2024-10-28 19:07:07 +0400

    Add shelve module

--- objs.py
+++ objs.py
@@ -1,3 +1,6 @@
+import shelve
+
+
 class User:
     def __init__(self, language):
         self.language = language
@@ -13,15 +16,18 @@ class User:
 
 class Users:
     def __init__(self):
-        self.users = {}
+        self._filename = 'users'
 
     def __getitem__(self, id):
-        if id in self.users:
-            return self.users[id]
-        raise KeyError(f'The user with this ID is not found')
+        with shelve.open(self._filename) as db:
+            if id in db:
+                return db[id]
+            else:
+                raise KeyError(f'The user with this ID={id} is not found')
 
     def __setitem__(self, id, user_obj):
         if isinstance(user_obj, User):
-            self.users[id] = user_obj
+            with shelve.open(self._filename) as db:
+                db[id] = user_obj
         else:
             raise TypeError('Value must be an instance of User class')
