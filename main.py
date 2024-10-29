89c7cf91d146aa1427d3f4cdca92a535f0eea8f5
Stanislav Provotorov <stas_provotorov@Stanislavs-MBP.home>
2024-10-29 16:17:36 +0400

    Delete global lang

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import telebot
 config = dotenv_values('tg_bot_token.env')
 TOKEN = config['TOKEN']
 
-bot_lang = 'en'
-
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    global bot_lang
     user_lang = message.from_user.language_code
-    bot_lang = user_lang
     message_back = {
         'en': 'Welcome! Your language is set to English',
         'ru': 'Добро пожаловать! Ваш язык установлен на русский'
