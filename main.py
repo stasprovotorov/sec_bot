f82466f92b6afbe758e746da009463d920944393
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2024-10-31 14:56:45 +0400

    Add Change language button

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from dotenv import dotenv_values
-import telebot
+from telebot import TeleBot, types
 from objs import Users, User
 import messages as msg
 
 config = dotenv_values('tg_bot_token.env')
 TOKEN = config['TOKEN']
 
-bot = telebot.TeleBot(TOKEN)
+bot = TeleBot(TOKEN)
 users = Users()
 
 
@@ -14,11 +14,14 @@ users = Users()
 def start(message):
     id = str(message.from_user.id)
     if id not in users:
-        language = 'ru' if message.from_user.language_code == 'ru' else 'en'
-        users[id] = User(language)
+        lang = 'ru' if message.from_user.language_code == 'ru' else 'en'
+        users[id] = User(lang)
     else:
-        language = users[id].language
-    bot.send_message(message.chat.id, msg.start[language])
+        lang = users[id].language
+    keyboard = types.InlineKeyboardMarkup()
+    change_lang_btn = types.InlineKeyboardButton(msg.change_lang_btn[lang], callback_data='change_language')
+    keyboard.add(change_lang_btn)
+    bot.send_message(message.chat.id, msg.start[lang], reply_markup=keyboard)
 
 
 bot.polling(none_stop=True)
--- messages.py
+++ messages.py
@@ -2,3 +2,8 @@ start = {
     'ru': 'Добро пожаловать! Для вас установлен русский язык',
     'en': 'Welcome! Your language is set to English'
 }
+
+change_lang_btn = {
+    'ru': 'Сменить язык',
+    'en': 'Change language'
+}
