611a05c1063d5e7118ec61d4ce827838cdebaeec
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2024-11-18 15:06:17 +0400

    Try to set main architecture

--- storage.py
+++ storage.py
@@ -8,67 +8,92 @@ class Storage(ABC):
     def __init__(self):
         if not hasattr(self, '_FILENAME') or not self._FILENAME:
             raise ValueError('Filename is not set in subclass')
+        
+    def get(self, key):
+        with shelve.open(self._FILENAME) as db:
+            return self._get(db, key)
 
-    @abstractmethod
-    def get(self):
-        pass
-
-    @abstractmethod
     def save(self):
         pass
 
-    @abstractmethod
     def edit(self):
         pass
 
-    @abstractmethod
     def delete(self):
+        pass
+
+    @abstractmethod
+    def _get(self):
+        pass
+
+    @abstractmethod
+    def _save(self):
+        pass
+
+    @abstractmethod
+    def _edit(self):
+        pass
+
+    @abstractmethod
+    def _delete(self):
         pass
 
 
 class StorageUser(Storage):
     _FILENAME = 'data_users'
 
-    def get(self):
+    def _get(self, db, key):
+        return key
+
+    def _save(self):
         raise NotImplementedError
 
-    def save(self):
+    def _edit(self):
         raise NotImplementedError
 
-    def edit(self):
+    def _delete(self):
         raise NotImplementedError
+    
+    def get_admins(self):
+        pass
 
-    def delete(self):
-        raise NotImplementedError
+    def add_admin(self):
+        pass
 
 
 class StorageButton(Storage):
     _FILENAME = 'data_buttons'
 
-    def get(self):
+    def _get(self, key):
         raise NotImplementedError
 
-    def save(self):
+    def _save(self):
         raise NotImplementedError
 
-    def edit(self):
+    def _edit(self):
         raise NotImplementedError
 
-    def delete(self):
+    def _delete(self):
         raise NotImplementedError
 
 
 class StorageContent(Storage):
     _FILENAME = 'data_content'
 
-    def get(self):
+    def _get(self, key):
         raise NotImplementedError
 
-    def save(self):
+    def _save(self):
         raise NotImplementedError
 
-    def edit(self):
+    def _edit(self):
         raise NotImplementedError
 
-    def delete(self):
+    def _delete(self):
         raise NotImplementedError
+
+
+if __name__ == '__main__':
+    stg_user = StorageUser()
+
+    stg_user.get(171025409)
