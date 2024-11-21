c6bf3b3a83cddc32b49767c0cbbfd95da62e5729
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2024-11-21 21:56:38 +0400

    Add StorageUsers methods

--- storage.py
+++ storage.py
@@ -21,26 +21,31 @@ class Storage():
 
 class StorageUsers(Storage):
     _FILENAME = 'data_users'
-    _default_structure = {
-        'users': dict, 
-        'admins': list
-    }
+    _default_structure = {'users': dict,  'admins': list}
+
+    def get_user(self, user_id):
+        with shelve.open(self._FILENAME) as db:
+            user_obj = db['users'].get(user_id)
+            if user_obj is None:
+                raise KeyError(f'User with this ID {user_id} not found')
+            return user_obj
+
+    def save_user(self, user_id, user_obj):
+        with shelve.open(self._FILENAME, writeback=True) as db:
+            db['users'][user_id] = user_obj
+
+    def del_user(self, user_id):
+        with shelve.open(self._FILENAME, writeback=True) as db:
+            user_obj = db['users'].pop(user_id, None)
+            if user_obj is None:
+                raise KeyError(f'User with this ID {user_id} not found')
 
 
 class StorageButtons(Storage):
     _FILENAME = 'data_buttons'
-    _default_structure = {
-        'buttons': dict
-    }
+    _default_structure = {'buttons': dict}
 
 
 class StorageContent(Storage):
     _FILENAME = 'data_content'
-    _default_structure = {
-        'text': dict,
-        'images': dict
-    }
-
-
-if __name__ == '__main__':
-    a = StorageUsers()
+    _default_structure = {'text': dict, 'images': dict}
