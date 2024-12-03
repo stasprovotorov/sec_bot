7325db6a8eed7445be998166e7e0b22e92500607
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2024-12-03 19:42:47 +0400

    Corrected __repr__

--- user.py
+++ user.py
@@ -82,5 +82,5 @@ class User:
         self.__dict__.update(state)
 
     def __repr__(self):
-        attrs = ', '.join([f"{k}={v!r}" for k, v in self.__dict__.items()])
+        attrs = ', '.join(f"{k}={v!r}" for k, v in self.__dict__.items())
         return f'{self.__class__.__name__}({attrs})'
