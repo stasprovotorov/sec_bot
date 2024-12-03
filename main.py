e8b3078520b4ae6485dcd8df89acf4cbfe6025a7
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2024-12-03 21:48:42 +0400

    Fix current state

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import dotenv_values
 from storage import StorageUsers
 from user import User
-from telebot import TeleBot
+from telebot import TeleBot, types
 
 config = dotenv_values('tg_bot_token.env')
 TOKEN = config['TOKEN']
@@ -13,8 +13,6 @@ users_db = StorageUsers()
 @bot.message_handler(commands=['start'])
 def start(message):
     pass
-    # user = User(users_db, message.chat.id, message.from_user.language_code)
-    # users_db.save_user(user)
 
 
 bot.polling(none_stop=True)
