482b31716adeff64da5314147c5f6c21e4cbc690
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2024-12-06 13:41:34 +0400

    Created Text cls

---
+++ message.py
@@ -0,0 +1,9 @@
+from user import Language
+
+class Text:
+    def __init__(self, content_ru, content_en):
+        self.lang = Language
+        self.content = {
+            self.lang.RU: content_ru,
+            self.lang.EN: content_en
+        }
