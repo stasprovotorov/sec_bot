71e8dc4b63a65e1738fa1e0f6c6b5d7f241a5889
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-01-08 00:49:15 +0400

    Resolved State problem and succesfuly get text from user

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from dotenv import dotenv_values
 from storage import StorageUsers, StorageContent
 from user import User, Language
-from telebot import TeleBot, types
+from telebot import TeleBot, types, custom_filters
+from telebot.handler_backends import State, StatesGroup
+from telebot.storage import StateMemoryStorage
 from view import Text, Image, View
 
 config = dotenv_values('tg_bot_token.env')
 TOKEN = config['TOKEN']
 
+storage = StateMemoryStorage()
+bot = TeleBot(TOKEN, state_storage=storage)
+bot.add_custom_filter(custom_filters.StateFilter(bot))
 
-bot = TeleBot(TOKEN)
 stg_users = StorageUsers()
 stg_content = StorageContent()
 vw = View(bot, stg_content)
+
+
+class EditorStates(StatesGroup):
+    state_text = State()
 
 
 @bot.message_handler(commands=['start'])
@@ -25,10 +33,17 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'edit')
 def edit(call):
-    pass
+    bot.set_state(call.from_user.id, EditorStates.state_text, call.message.chat.id)
+    bot.send_message(call.message.chat.id, 'Type text, please!')
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.message_handler(state=EditorStates.state_text, content_types=['text'])
+def get_text_from_user(message):
+    print(message.text)
+    bot.delete_state(message.from_user.id)
+
+
+@bot.callback_query_handler(func=lambda call: call.data != 'edit')
 def buttons_callback(call):
     content_key = call.data
     user = User(stg_users, call.message.from_user.id, call.message.from_user.language_code)
