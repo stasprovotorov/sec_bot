d37fd33909cea451dd137ee25873656b85cadcac
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-03-04 14:18:34 +0400

    Refactored and commented state storage and Telegram bot initializing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from dotenv import dotenv_values
+from telebot.storage import StateMemoryStorage
 
 from storage import StorageUsers, StorageContent
 from user import User
 from telebot import TeleBot, types, custom_filters
-from telebot.storage import StateMemoryStorage
 from view import View
 from editor import Editor, StatesViewEdit
 from editor_data import editor_msg, editor_btn, state_group_to_content_type
@@ -12,9 +12,10 @@ from editor_data import editor_msg, editor_btn, state_group_to_content_type
 config = dotenv_values('config.env')
 TELEGRAM_BOT_TOKEN = config['TELEGRAM_BOT_TOKEN']
 
+# Initialize the Telegram bot object with a StateMemoryStorage object for storing user states
+state_storage = StateMemoryStorage()
+bot = TeleBot(TELEGRAM_BOT_TOKEN, state_storage=state_storage)
 
-storage = StateMemoryStorage()
-bot = TeleBot(TELEGRAM_BOT_TOKEN, state_storage=storage)
 bot.add_custom_filter(custom_filters.StateFilter(bot))
 
 stg_users = StorageUsers()
