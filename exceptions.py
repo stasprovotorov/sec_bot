18d06e107f346652cbcf83acd472507fb853c0c2
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-03-08 13:50:16 +0400

    Created exception class UserNotFoundError

--- exceptions.py
+++ exceptions.py
@@ -10,6 +10,15 @@ class StorageUsersError(StorageError):
     pass
 
 
+class UserNotFoundError(StorageUsersError):
+    '''Exception raised when a user is not found in persistent storage'''
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.error_message = f'User with ID {self.user_id} is not found'
+        super().__init__(self.error_message)
+
+
 class UserAlreadyExistsError(StorageUsersError):
     '''Exception raised when a user already exists in persistent storage'''
     
