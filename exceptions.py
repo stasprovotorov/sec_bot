761406af355ca322f2fae627d32b08d85e8754cc
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-03-11 23:50:18 +0400

    Fix mistake in class name ImageNameAlreadyExistsError

--- exceptions.py
+++ exceptions.py
@@ -76,7 +76,7 @@ class ImageNameNotFoundError(StorageTextsError):
         super().__init__(f'Image with name {image_name} is not found')
 
 
-class ImagetNameAlreadyExistsError(StorageTextsError):
+class ImageNameAlreadyExistsError(StorageTextsError):
     '''Exception raised when a image with the given name already exists in persistent storage'''
     def __init__(self, image_name: str) -> None:
         super().__init__(f'Image with name {image_name} already exists')
