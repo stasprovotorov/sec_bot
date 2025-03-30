023ae96d71bc9645b7fb6aefcdb9ff286aa85dfd
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-03-31 00:01:06 +0400

    Added dictionary CALLBACK_MESSAGES without message values yet

--- editor_messages.py
+++ editor_messages.py
@@ -3,7 +3,7 @@ This is a module for retrieving text that is sent in a dialogue with the user wh
 '''
 
 
-def get_message(callback_data: str) -> str:
+def get_message(callback_data: str, message_language: str) -> str:
     '''A function to get a message from callback_messages using callback_data.'''
 
     _, *callback_keys = callback_data.split(':')
@@ -14,13 +14,138 @@ def get_message(callback_data: str) -> str:
 
         result = callback_messages[callback_key]
 
-        if isinstance(result, str):
-            return result
-        elif isinstance(result, dict):
+        try:
+            return result[message_language]
+        except KeyError:
             index += 1
             return recursion(result, callback_keys[index])
 
-    return recursion(callback_messages, callback_keys[0])
+    return recursion(CALLBACK_MESSAGES, callback_keys[0])
 
 
-callback_messages = {}
+CALLBACK_MESSAGES = {
+    'menu': {
+        'ru': None,
+        'en': None
+    },
+    'text': {
+        'action': {
+            'ru': None,
+            'en': None
+        },
+        'create': {
+            'ru': None,
+            'en': None
+        },
+        'edit': {
+            'ru': None,
+            'en': None
+        },
+        'delete': {
+            'ru': None,
+            'en': None
+        }
+    },
+    'image': {
+        'action': {
+            'ru': None,
+            'en': None
+        },
+        'load': {
+            'ru': None,
+            'en': None
+        },
+        'delete': {
+            'ru': None,
+            'en': None
+        }
+    },
+    'button': {
+        'action': {
+            'ru': None,
+            'en': None
+        },
+        'create': {
+            'ru': None,
+            'en': None
+        },
+        'edit': {
+            'component': {
+                'ru': None,
+                'en': None
+            },
+            'label': {
+                'ru': None,
+                'en': None
+            },
+            'message': {
+                'ru': None,
+                'en': None
+            }
+        },
+        'delete': {
+            'ru': None,
+            'en': None
+        }
+    },
+    'message': {
+        'action': {
+            'ru': None,
+            'en': None
+        },
+        'create': {
+            'ru': None,
+            'en': None
+        },
+        'edit': {
+            'component': {
+                'ru': None,
+                'en': None
+            },
+            'text': {
+                'ru': None,
+                'en': None
+            },
+            'button': {
+                'action': {
+                    'ru': None,
+                    'en': None
+                },
+                'add': {
+                    'ru': None,
+                    'en': None
+                },
+                'replace': {
+                    'ru': None,
+                    'en': None
+                },
+                'delete': {
+                    'ru': None,
+                    'en': None
+                }
+            },
+            'image': {
+                'action': {
+                    'ru': None,
+                    'en': None
+                },
+                'add': {
+                    'ru': None,
+                    'en': None
+                },
+                'replace': {
+                    'ru': None,
+                    'en': None
+                },
+                'delete': {
+                    'ru': None,
+                    'en': None
+                }
+            }
+        },
+        'delete': {
+            'ru': None,
+            'en': None
+        }
+    }
+}
