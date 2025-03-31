e2e185b5347d2eeb28f375a10113c2f9f029b73c
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2025-03-31 19:25:35 +0400

    Added messages in Russian

--- editor_messages.py
+++ editor_messages.py
@@ -29,350 +29,284 @@ def get_message(user_action: str, message_language: str) -> str:
 
 EDITOR_MESSAGES = {
     'menu': {
-        'ru': None,
+        'ru': 'Выберите компонент бота для редактирования',
         'en': None
     },
     'text': {
         'action': {
-            'ru': None,
-            'en': None
-        },
-        'create': {
-            'ru': None,
-            'en': None
-        },
-        'edit': {
-            'ru': None,
-            'en': None
-        },
-        'delete': {
-            'ru': None,
+            'ru': 'Выберите действие с текстом',
             'en': None
         }
     },
     'image': {
         'action': {
-            'ru': None,
-            'en': None
-        },
-        'load': {
-            'ru': None,
-            'en': None
-        },
-        'delete': {
-            'ru': None,
+            'ru': 'Выберите действие с изображением',
             'en': None
         }
     },
     'button': {
         'action': {
-            'ru': None,
-            'en': None
-        },
-        'create': {
-            'ru': None,
+            'ru': 'Выберите действие с кнопкой',
             'en': None
         },
         'edit': {
             'component': {
-                'ru': None,
-                'en': None
-            },
-            'label': {
-                'ru': None,
-                'en': None
-            },
-            'message': {
-                'ru': None,
+                'ru': 'Выберите компонент кнопки, который вы желаете отредактировать',
                 'en': None
             }
-        },
-        'delete': {
-            'ru': None,
-            'en': None
         }
     },
     'message': {
         'action': {
-            'ru': None,
-            'en': None
-        },
-        'create': {
-            'ru': None,
+            'ru': 'Выберите действие с сообщением',
             'en': None
         },
         'edit': {
             'component': {
-                'ru': None,
-                'en': None
-            },
-            'text': {
-                'ru': None,
+                'ru': 'Выберите компонент сообщения, который вы желаете отредактировать',
                 'en': None
             },
             'button': {
                 'action': {
-                    'ru': None,
+                    'ru': 'Выберите действие, которые вы желаете выполнить с кнопкой сообщения',
                     'en': None
                 },
-                'add': {
-                    'ru': None,
-                    'en': None
-                },
-                'replace': {
-                    'ru': None,
-                    'en': None
-                },
-                'delete': {
-                    'ru': None,
-                    'en': None
-                }
             },
             'image': {
                 'action': {
-                    'ru': None,
+                    'ru': 'Выберите действие, которые вы желаете выполнить с изображением сообщения',
                     'en': None
                 },
-                'add': {
-                    'ru': None,
-                    'en': None
-                },
-                'replace': {
-                    'ru': None,
-                    'en': None
-                },
-                'delete': {
-                    'ru': None,
-                    'en': None
-                }
             }
         },
-        'delete': {
-            'ru': None,
+    },
+    'role': {
+        'action':{
+            'ru': 'Выберите действие для роли у пользователя',
             'en': None
         }
     },
     'StatesEditorTextCreate': {
         'enter_text_name': {
-            'ru': None,
+            'ru': 'Введите наименование для нового текста',
             'en': None
         },
         'enter_text_ru': {
-            'ru': None,
+            'ru': 'Введите текст на русском языке',
             'en': None
         },
         'enter_text_en': {
-            'ru': None,
+            'ru': 'Введите текст на английском языке',
             'en': None
         }
     },
     'StatesEditorTextEdit': {
         'choose_text_name': {
-            'ru': None,
+            'ru': 'Выберите наименование текста, который вы желаете отредактировать',
             'en': None
         },
         'choose_text_language': {
-            'ru': None,
+            'ru': 'Выберите язык текста',
             'en': None
         },
         'enter_text': {
-            'ru': None,
+            'ru': 'Введите текст',
             'en': None
         }
     },
     'StatesEditorTextDelete': {
         'choose_text_name': {
-            'ru': None,
+            'ru': 'Выберите наименование текста, который вы желаете удалить',
             'en': None
         },
         'choose_text_language': {
-            'ru': None,
+            'ru': 'Выберите язык текста, который вы желаете удалить',
             'en': None
         }
     },
     'StatesEditorImageLoad': {
         'enter_image_name': {
-            'ru': None,
+            'ru': 'Введите наименование для загружаемого изображения',
             'en': None
         },
         'attach_image': {
-            'ru': None,
+            'ru': 'Отправьте изображение',
             'en': None
         }
     },
-    'StatesEditorImageLoad': {
+    'StatesEditorImageDelete': {
         'choose_image_name': {
-            'ru': None,
+            'ru': 'Введите наименование изображения, которое вы желаете удалить',
             'en': None
         }
     },
     'StatesEditorButtonCreate': {
         'enter_button_name': {
-            'ru': None,
+            'ru': 'Введите наименование для новой кнопки',
             'en': None
         },
         'enter_button_label_ru': {
-            'ru': None,
+            'ru': 'Введите значение лейбла кнопки на русском языке',
             'en': None
         },
         'enter_button_label_en': {
-            'ru': None,
+            'ru': 'Введите значение лейбла кнопки на английском языке',
             'en': None
         },
-        'enter_button_message': {
-            'ru': None,
+        'enter_button_message_call': {
+            'ru': 'Введите значение вызова сообщения у кнопки',
             'en': None
         }
     },
     'StatesEditorButtonEditLabel': {
         'choose_button_name': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки, лейбл которой вы желаете отредактировать',
             'en': None
         },
         'choose_button_label_language': {
-            'ru': None,
+            'ru': 'Выберите язык лейбла кнопки, который вы желаете отредактировать',
             'en': None
         },
         'enter_button_label': {
-            'ru': None,
+            'ru': 'Введите новое значение лейбла кнопки',
             'en': None
         }
     },
-    'StatesEditorButtonEditMessage': {
+    'StatesEditorButtonEditMessageCall': {
         'choose_button_name': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки, вызов сообщения которой вы желаете отредактировать',
             'en': None
         },
         'enter_button_message': {
-            'ru': None,
+            'ru': 'Введите значение вызова сообщения для кнопки',
             'en': None
         }
     },
     'StatesEditorButtonDelete': {
         'choose_button_name': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки, которую вы желаете удалить',
             'en': None
         }
     },
     'StatesEditorMessageCreate': {
         'enter_message_name': {
-            'ru': None,
+            'ru': 'Введите наименование для нового сообщения',
             'en': None
         },
         'choose_message_text_name': {
-            'ru': None,
+            'ru': 'Выберите наименование текста, который будет в сообщении',
             'en': None
         },
         'choose_message_image_name': {
-            'ru': None,
+            'ru': 'Выберите наименование изображения, которое будет в сообщении',
             'en': None
         },
         'choose_message_button_name': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопок, которые будут в сообщении',
             'en': None
         }
     },
     'StatesEditorMessageEditTextReplace': {
         'enter_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, текст которого вы желаете заменить',
             'en': None
         },
         'choose_message_text_name_added': {
-            'ru': None,
+            'ru': 'Выберите наименование текста, на который вы хотите сделать замену',
             'en': None
         }
     },
     'StatesEditorMessageEditButtonAdd': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, к которому вы желаете добавить кнопку',
             'en': None
         },
         'choose_message_button_name_added': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки, которую хотите добавить в сообщение',
             'en': None
         }
     },
     'StatesEditorMessageEditButtonReplace': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, у которого вы желаете заменить кнопку',
             'en': None
         },
         'choose_message_button_name_current': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки сообщения, которую вы желаете заменить',
             'en': None
         },
         'choose_message_button_name_added': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки, на которую вы желаете сделать замену',
             'en': None
         }
     },
     'StatesEditorMessageEditButtonDelete': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, у которого вы желаете удалить кнопку',
             'en': None
         },
         'choose_message_button_name': {
-            'ru': None,
+            'ru': 'Выберите наименование кнопки сообщения, которую вы желаете удалить',
             'en': None
         }
     },
     'StatesEditorMessageEditImageAdd': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, к которому вы желаете добавить изображение',
             'en': None
         },
         'choose_message_image_name': {
-            'ru': None,
+            'ru': 'Выберите наименование изображения, которое вы желаете добавить в сообщение',
             'en': None
         }
     },
     'StatesEditorMessageEditImageReplace': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, у которого вы желаете заменить изображение',
             'en': None
         },
         'choose_message_image_name_current': {
-            'ru': None,
+            'ru': 'Выберите наименование изображения сообщения, которое вы желаете заменить',
             'en': None
         },
         'choose_message_image_name_added': {
-            'ru': None,
+            'ru': 'Выберите наименование изображения, на которое вы желаете сделать замену',
             'en': None
         }
     },
     'StatesEditorMessageEditImageDelete': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, у которого вы желаете удалить изображение',
             'en': None
         },
         'choose_message_image_name': {
-            'ru': None,
+            'ru': 'Выберите наименование изображения сообщения, которое вы желаете удалить',
             'en': None
         }
     },
     'StatesEditorMessageDelete': {
         'choose_message_name': {
-            'ru': None,
+            'ru': 'Выберите наименование сообщения, которое вы желаете удалить',
             'en': None
         }
     },
     'StatesEditorRolesAssign': {
         'attach_contact': {
-            'ru': None,
+            'ru': 'Пришлите контакт пользователя в приложении',
             'en': None
         },
         'choose_role_added': {
-            'ru': None,
+            'ru': 'Выберите роль, которую вы желаете добавить пользователю',
             'en': None
         }
     },
     'StatesEditorRolesRemove': {
         'attach_contact': {
-            'ru': None,
+            'ru': 'Пришлите контакт пользователя в приложении',
             'en': None
         },
         'choose_role_current': {
-            'ru': None,
+            'ru': 'Выберите роль, которую вы желаете убрать у пользователя',
             'en': None
         }
     }
